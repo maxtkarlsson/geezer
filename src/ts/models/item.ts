0c440b8d339cd37ec42c4b80665c50ca9be81442
maxtkarlsson <max.karlsson93@gmail.com>
2022-12-22 15:28:06 +0100

    removed cart.ts

---
+++ src/ts/models/item.ts
@@ -0,0 +1,33 @@
+export class Item {
+  articleNumber: Number;
+  title: String;
+  imageUrl: String;
+  desc: String;
+  price: Number;
+  size: string;
+  sizeValue: number;
+  infoBtn: string;
+  addBtn: string;
+
+  constructor(
+    articleNumber: Number,
+    title: string,
+    imageUrl: string,
+    desc: string,
+    price: number,
+    size: string,
+    sizeValue: number,
+    infoBtn: string,
+    addBtn: string
+  ) {
+    this.articleNumber = articleNumber;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.desc = desc;
+    this.price = price;
+    this.size = size;
+    this.sizeValue = sizeValue;
+    this.infoBtn = infoBtn;
+    this.addBtn = addBtn;
+  }
+}
