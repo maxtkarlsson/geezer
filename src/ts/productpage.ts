351b71bbe60707881a56110c60cf1fe483e5cd84
maxtkarlsson <max.karlsson93@gmail.com>
2022-12-23 14:16:00 +0100

    createHTMLProductlist fn now writes HTML from list

--- src/ts/productpage.ts
+++ src/ts/productpage.ts
@@ -10,7 +10,6 @@ function createHTMLProductlist(products: Item[]) {
     let itemDesc = document.createElement("p") as HTMLParagraphElement;
     let itemArticleNumber = document.createElement("p") as HTMLParagraphElement;
     let itemSize = document.createElement("p") as HTMLParagraphElement;
-
     let itemPrice = document.createElement("p") as HTMLParagraphElement;
 
     //Adds classes to the elements
@@ -19,20 +18,35 @@ function createHTMLProductlist(products: Item[]) {
     itemTitle.classList.add("itemCard__title");
     itemDesc.classList.add("itemCard__desc");
     itemArticleNumber.classList.add("itemCard__articleNumber");
+    itemPrice.classList.add("itemCard__price");
     itemSize.classList.add("itemCard__size");
 
     //Adds id
 
     //Adds content to the elements
-    //itemImg.innerHTML =
+    itemImg.src =
+      "https://dl.dropboxusercontent.com/s/owlgd106tqi7t2h/200x200.png?dl=0";
+    itemImg.alt = "";
     itemTitle.innerText = products[i].title;
     itemDesc.innerText = products[i].desc;
-    itemArticleNumber.innerText = products[i].articleNumber;
-    itemSize.innerText = products[i].size;
+    itemPrice.innerText = products[i].price.toString() + " kr";
+    itemArticleNumber.innerText =
+      "Artikel nummer: " + products[i].articleNumber;
+    itemSize.innerText = "Storlek: " + products[i].size;
 
     //Adds elements to page
-    let flexContainer = document.getElementsByClassName("flexContainer");
+    let flexContainer = document.getElementById(
+      "flexContainer"
+    ) as HTMLDivElement;
 
-    itemContainer.appendChild;
+    flexContainer.appendChild(itemContainer);
+    itemContainer.appendChild(itemImg);
+    itemContainer.appendChild(itemTitle);
+    itemContainer.appendChild(itemDesc);
+    itemContainer.appendChild(itemArticleNumber);
+    itemContainer.appendChild(itemPrice);
+    itemContainer.appendChild(itemSize);
   }
 }
+
+createHTMLProductlist(products);
